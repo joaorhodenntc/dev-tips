1e422ac5f4b4d74a583a70943c3aa12f957ae9cc
João Rhoden <jvrhodenmachado@hotmail.com>
2023-12-07 01:33:54 -0200

    att

---
+++ pages/cadastro/script.js
@@ -0,0 +1,24 @@
+function toggleContent(showId, hideId) {
+    document.getElementById(showId).style.display = 'flex';
+    document.getElementById(hideId).style.display = 'none';
+  }
+
+  function verificarPreenchimento() {
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    var confirmarSenha = document.getElementById('confirmarSenha').value;
+    var avisoElement = document.getElementById('aviso');
+    var infosInscrever = document.getElementById('inscrever-content');
+
+    if (email.trim() === '' || senha.trim() === '' || confirmarSenha.trim() === '') {
+      avisoElement.textContent = 'Preencha todos os campos antes de prosseguir.';
+      infosInscrever.classList.add('shake'); // Adicionar classe de animação
+      setTimeout(function() {
+        infosInscrever.classList.remove('shake'); // Remover classe após a animação
+      }, 500);
+    } else if (senha !== confirmarSenha){
+      avisoElement.textContent = 'As senhas não coincidem.';
+    } else {
+      toggleContent('infos-sucesso', 'infos-inscrever');
+    }
+  }
