d0cce333c52c031fec0befa5cc22ba2452d46327
santosdasilvag <contatogsantosdasilva@gmail.com>
2023-12-08 22:27:20 -0300

    sidebar/att contato

--- pages/contato/script.js
+++ pages/contato/script.js
@@ -1,32 +1,3 @@
-function toggleContent(showId, hideId) {
-    document.getElementById(showId).style.display = 'flex';
-    document.getElementById(hideId).style.display = 'none';
-  }
-
-  function verificarPreenchimento() {
-    var email = document.getElementById('email').value;
-    var senha = document.getElementById('senha').value;
-    var confirmarSenha = document.getElementById('confirmarSenha').value;
-    var avisoElement = document.getElementById('aviso');
-    var infosInscrever = document.getElementById('inscrever-content');
-
-    if (email.trim() === '' || senha.trim() === '' || confirmarSenha.trim() === '') {
-      avisoElement.textContent = 'Preencha todos os campos antes de prosseguir.';
-      infosInscrever.classList.add('shake'); 
-      setTimeout(function() {
-        infosInscrever.classList.remove('shake');
-      }, 500);
-    } else if (senha !== confirmarSenha){
-      avisoElement.textContent = 'As senhas não coincidem.';
-    } else {
-      toggleContent('infos-sucesso', 'infos-inscrever');
-    }
-  }
-
-  document.addEventListener("DOMContentLoaded", function () {
-    highlightActiveIcon();
-});
-
 function highlightActiveIcon() {
     // Pega a URL da página atual
     var currentPageUrl = window.location.pathname;
